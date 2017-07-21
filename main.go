c3633cae4c4acd106e85a39b45c9a697a0124846
Kim Pettersen <kim.a.pettersen@gmail.com>
2017-07-21 20:01:25 +0100

    Added passing tests for moving backwards heading 4 directions

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ type Rover struct {
 func (r *Rover) moveBackwards() {
 	if r.heading == "N" {
 		r.y = r.y - 1
+	} else if r.heading == "E" {
+		r.x = r.x - 1
+	} else if r.heading == "S" {
+		r.y = r.y + 1
+	} else if r.heading == "W" {
+		r.x = r.x + 1
 	}
 }
 
--- main_test.go
+++ main_test.go
@@ -40,4 +40,22 @@ func TestMovingBackward(t *testing.T) {
 		assert.Equal(t, rover.y, 0, "it should move one step down the y-axis")
 		assert.Equal(t, rover.x, 1, "it should stay in the same position on x-axis")
 	})
+	t.Run("When Facing East", func(t *testing.T) {
+		rover := Rover{1, 1, "E"}
+		rover.Move("B")
+		assert.Equal(t, rover.y, 1, "it should stay in the same position on y-axis")
+		assert.Equal(t, rover.x, 0, "it should move one step down the x-axis")
+	})
+	t.Run("When Facing South", func(t *testing.T) {
+		rover := Rover{0, 0, "S"}
+		rover.Move("B")
+		assert.Equal(t, rover.y, 1, "it should move one step up the y-axis")
+		assert.Equal(t, rover.x, 0, "it should stay in the same position on y-axis")
+	})
+	t.Run("When Facing West", func(t *testing.T) {
+		rover := Rover{0, 0, "W"}
+		rover.Move("B")
+		assert.Equal(t, rover.y, 0, "it should stay in the same position on y-axis")
+		assert.Equal(t, rover.x, 1, "it should move one step up the x-axis")
+	})
 }
